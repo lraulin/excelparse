cca6160cb7aa041c8d6033d9c4fe38029059a7f8
Lee Raulin <leeraulin@gmail.com>
2019-05-01 15:25:55 -0400

    moved git folder

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='excelparse',
+    version='0.0.1',
+    entry_points={
+        'console_scripts': ['excelparse=excelparse.main:main'],
+    },
+    author="Lee Raulin",
+    author_email="leeraulin@gmail.com",
+    description="Show current weather from OpenWeather API in console.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lraulin/pyweather",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
