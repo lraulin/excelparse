c39a9bfba9ad674ff7b40941854ad42bd5d59e8f
Lee Raulin <leeraulin@gmail.com>
2019-05-07 15:47:46 -0400

    Check url redirect

--- excelparse/main.py
+++ excelparse/main.py
@@ -2,46 +2,75 @@ import os
 import pandas as pd
 import requests
 import urllib3
+from shutil import copyfile
+import sys
+import requests
+from openpyxl import Workbook, load_workbook
+import json
 
-def save_to_xlsx(df):
-    writer = pd.ExcelWriter('example.xlsx', engine='xlsxwriter', options={'strings_to_urls': False})
-    df.to_excel(writer, 'Sheet1')
+TIMEOUT = 15
+FILE_NAME = "FOUO-USDOT-website list 20190413.xlsx"
+SHEET_NAME = "Source-Live Websites w Owners"
+OUTPUT_FILE = "your_file.txt"
 
-def request_status(url):
-    print(f'Checking {url}...')
-    try: 
-        r = requests.get(url, timeout=5)
-        print(r.status_code)
-        return r.status_code
+def save_to_xlsx():
+    wb = load_workbook(FILE_NAME)
+    sheet = wb[SHEET_NAME]
+
+def check_for_redirects(url):
+    print(f"Checking {url}...")
+    result = {
+        "url": url,
+        "status_code": None,
+        "message": None,
+        "r_url": None
+    }
+    try:
+        r = requests.get(url, allow_redirects=False, timeout=TIMEOUT)
+        result["status_code"] = r.status_code
+        print(f"Status Code: {r.status_code}")
+        if 300 <= r.status_code < 400:
+            result["r_url"] = r.headers['location']
+            print(f"Location: {r.headers['location']}")
+        else:
+            result["message"] = '[no redirect]'
+            print('No Redirect')
+    except requests.exceptions.Timeout:
+        print("Timeout")
+        result["message"] = f'[timeout after {TIMEOUT} s]'
     except requests.exceptions.ConnectionError:
-        print("ConnectionError")
-        return "FAIL"
-    except urllib3.exceptions.ReadTimeoutError:
-        print("Timed out")
-        return "TIMEOUT"
-    except:
-        pass
+        print("Connection Error")
+        result["message"] = '[connection error]'
+    finally:
+        return result
+
 
 def write_list(my_list):
     with open('your_file.txt', 'w') as f:
         for item in my_list:
             f.write("%s\n" % item)
 
-def check_websites():
-    file = "FOUO-USDOT-website list 20190413.xlsx"
-    xl = pd.ExcelFile(file)
-    df = xl.parse('Source-Live Websites w Owners')
-    column = df.loc[:, 'Canonical URL']
+def check_websites(column_header):
+    xl_file = pd.ExcelFile(FILE_NAME)
+    data_frame = xl_file.parse('Source-Live Websites w Owners')
+    column = data_frame.loc[:, column_header]
     urls = [x for x in column]
-    statuses = [request_status(x) for x in urls]
-    live = ['Yes' if x == 200 else 'No' for x in statuses]
-    write_list(live)
-    return live
+    statuses = [check_for_redirects(x) for x in urls]
+    return statuses
+
+def modify_xlsx():
+    lines = [line.rstrip('\n') for line in open(OUTPUT_FILE)]
+    xl_file = pd.ExcelFile(FILE_NAME)
+    data_frame = xl_file.parse(SHEET_NAME)
+    data_frame.insert(4, 'Live', lines)
+    writer = pd.ExcelWriter(FILE_NAME, engine='xlsxwriter', options={'strings_to_urls': False})
+    data_frame.to_excel(writer, SHEET_NAME)
 
 def main():
-    statuses = check_websites()
-    df.insert(4, 'Live', statuses)
-    save_to_xlsx(df)
+    statuses = check_websites('Canonical URL')
+    with open('url_responses.json', 'w') as f:
+        json.dump(statuses, f)
+    # save_to_xlsx()
 
 if __name__ == "__main__":
     main()
