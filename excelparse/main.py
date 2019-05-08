d66db6276ffb7def358de7d986e51916cce07a74
Lee Raulin <leeraulin@gmail.com>
2019-05-08 09:42:56 -0400

    Modify excel file

--- excelparse/main.py
+++ excelparse/main.py
@@ -11,11 +11,10 @@ import json
 TIMEOUT = 15
 FILE_NAME = "FOUO-USDOT-website list 20190413.xlsx"
 SHEET_NAME = "Source-Live Websites w Owners"
-OUTPUT_FILE = "your_file.txt"
+OUTPUT_FILE = "url_responses.json"
 
-def save_to_xlsx():
-    wb = load_workbook(FILE_NAME)
-    sheet = wb[SHEET_NAME]
+
+
 
 def check_for_redirects(url):
     print(f"Checking {url}...")
@@ -58,19 +57,53 @@ def check_websites(column_header):
     statuses = [check_for_redirects(x) for x in urls]
     return statuses
 
+
+def create_live_col(sheet, arr):
+    sheet.insert_cols(5)
+    sheet.cell(column=5, row=1, value='Live')
+    for i in range(0, len(arr)):
+        sheet.cell(column=5, row=i+2, value=arr[i])
+
+def update_redirect_col(sheet, arr):
+    for i in range(len(arr)):
+        is_redirect = False if arr[i] == None else True
+        url = "" if arr[i] == None else arr[i]
+        sheet.cell(column=6, row=i+2, value=is_redirect)
+        sheet.cell(column=7, row=i+2, value=url)
+
+
 def modify_xlsx():
-    lines = [line.rstrip('\n') for line in open(OUTPUT_FILE)]
-    xl_file = pd.ExcelFile(FILE_NAME)
-    data_frame = xl_file.parse(SHEET_NAME)
-    data_frame.insert(4, 'Live', lines)
-    writer = pd.ExcelWriter(FILE_NAME, engine='xlsxwriter', options={'strings_to_urls': False})
-    data_frame.to_excel(writer, SHEET_NAME)
+    # Get data
+    data = load_json(OUTPUT_FILE)
+    live = []
+    redirect = []
+    for item in data:
+        live.append(
+            True if (item['status_code'] == 200 or item['r_url'] != None)
+            else False)
+        redirect.append(item['r_url'])
+
+    # Save data
+    wb = load_workbook(FILE_NAME)
+    sheet = wb[SHEET_NAME]
+    create_live_col(sheet, live)
+    update_redirect_col(sheet, redirect)
+    wb.save(FILE_NAME)
+
+
+def save_json(data, file):
+    with open(file, 'w') as f:
+        json.dump(data, f)
+
+def load_json(file):
+    with open(file, 'r') as f:
+        return json.load(f)
+
 
 def main():
     statuses = check_websites('Canonical URL')
-    with open('url_responses.json', 'w') as f:
-        json.dump(statuses, f)
-    # save_to_xlsx()
+    save_json(statuses)
+    modify_xlsx()
 
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -16,6 +16,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lraulin/excelparse",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'openpyxl',
+        'pandas'
+    ]
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
